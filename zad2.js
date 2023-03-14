dca14728b44e2a6a18d0bf890f06d6b86c4a55b4
Pawciu <pawstrup@gmail.com>
2023-03-14 21:12:59 +0100

    zad2 corrected

--- zad2.js
+++ zad2.js
@@ -2,31 +2,40 @@ const age = [25, 50, 75, 100, 200];
 
 //Callback functions
 
-const NumberToPowerTwo = (x) => Math.pow(x, 2);
+const calculateNumberToPowerTwo = (x) => Math.pow(x, 2);
 const isAdult = (element) => element > 25;
 const reduceCallback = (total, num) => total - num;
 const everyCallback = (parameter) => parameter < 30;
 
 //For Each
 const forEachFn = (array, callback) => {
-  for (i = 0; i < array.length; i++) {
+  if (!Array.isArray(array) || typeof callback !== "function") {
+    return "incorrect input";
+  }
+  for (const i in array) {
     callback(array[i]);
   }
 };
 
-console.log("For Each function returns:", forEachFn(age, NumberToPowerTwo));
+console.log(
+  "For Each function returns:",
+  forEachFn(age, calculateNumberToPowerTwo)
+);
 
 //Map
 
 const mapFn = (array, callback) => {
+  if (!Array.isArray(array) || typeof callback !== "function") {
+    return "incorrect input";
+  }
   const mappedArray = [];
-  for (i = 0; i < array.length; i++) {
+  for (const i in array) {
     mappedArray.push(callback(array[i]));
   }
   return mappedArray;
 };
 
-console.log("Map function returns:", mapFn(age, NumberToPowerTwo));
+console.log("Map function returns:", mapFn(age, calculateNumberToPowerTwo));
 
 //Entries
 
@@ -43,8 +52,11 @@ console.log("Map function returns:", mapFn(age, NumberToPowerTwo));
 //Filter
 
 const filterFn = (array, callback) => {
+  if (!Array.isArray(array) || typeof callback !== "function") {
+    return "incorrect input";
+  }
   const filteredArray = [];
-  for (i = 0; i < array.length; i++) {
+  for (const i in array) {
     if (callback(array[i])) {
       filteredArray.push(array[i]);
     }
@@ -56,15 +68,34 @@ console.log("Filter function returns:", filterFn(age, isAdult));
 
 //Reduce
 
-const reduceFn = (array, callback, initial) => {
-  let reduced = 0;
-  let i = 0;
-  while (array.length > i) {
-    reduced = callback(initial, array[i]);
-    initial = reduced;
-    i++;
+const reduceFn = function (array, callback, initial) {
+  if (!Array.isArray(array) || typeof callback !== "function") {
+    return "incorrect input";
   }
-  return reduced;
+
+  let reduced = 0;
+
+  if (Array.from(arguments).includes(initial)) {
+    if (typeof initial !== "number") {
+      return "incorrect input";
+    }
+    let i = 0;
+    while (array.length > i) {
+      reduced = callback(initial, array[i]);
+      initial = reduced;
+      i++;
+    }
+    return reduced;
+  } else {
+    let newInitial = array[0];
+    let i = 1;
+    while (array.length > i) {
+      reduced = callback(newInitial, array[i]);
+      newInitial = reduced;
+      i++;
+    }
+    return reduced;
+  }
 };
 
 console.log("Reduce function returns:", reduceFn(age, reduceCallback, 1000));
@@ -72,10 +103,13 @@ console.log("Reduce function returns:", reduceFn(age, reduceCallback, 1000));
 //Every
 
 const everyFn = (array, callback) => {
+  if (!Array.isArray(array) || typeof callback !== "function") {
+    return "incorrect input";
+  }
   let statement = Boolean();
-  for (i = 0; i < array.length; i++) {
+  for (const i in array) {
     statement = Boolean(callback(array[i]));
-    if (statement == 0) {
+    if (!statement) {
       break;
     }
   }
@@ -87,10 +121,13 @@ console.log("Every function returns:", everyFn(age, everyCallback));
 //Some
 
 const someFn = (array, callback) => {
+  if (!Array.isArray(array) || typeof callback !== "function") {
+    return "incorrect input";
+  }
   let statement = Boolean();
-  for (i = 0; i < array.length; i++) {
+  for (const i in array) {
     statement = Boolean(callback(array[i]));
-    if (statement == 1) {
+    if (statement) {
       break;
     }
   }
